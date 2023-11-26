f8549c528221ded216a17ce89e2d73be1b0a8efd
Viktor Rudenko <darthvitek@gmail.com>
2023-11-26 19:20:25 +0200

    23. Literal Types

--- 02-basics-and-basic-types/ts-section-2/src/main.ts
+++ 02-basics-and-basic-types/ts-section-2/src/main.ts
@@ -1,6 +1,8 @@
 // import basics from "./basics";
 // import objects from "./types/objects";
-import enums from "./types/enums";
+// import enums from "./types/enums";
+
+import union from "./types/union";
 
 
-enums();
+union();
