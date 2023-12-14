68d13e39fb1ca5967d314fc70fe3781feaf79598
Viktor <v.rudenko@triacom.ua>
2023-12-14 13:29:58 +0200

    61. Constructor Functions & The "this" Keyword

--- 05-classes-and-interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ 05-classes-and-interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,1 +1,22 @@
 // Code goes here!
+
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
+}
+
+const accounting = new Department("Accounting");
+
+accounting.describe();
+
+
+const accountingCopy = { describe: accounting.describe, name: "baboon" };
+
+accountingCopy.describe()
