1e7324468538b16be1cd17632088542daa34db24
Viktor <v.rudenko@triacom.ua>
2023-12-14 14:15:47 +0200

    63. Shorthand Initialization

--- 05-classes-and-interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ 05-classes-and-interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -2,6 +2,7 @@
 
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -10,13 +11,23 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+// accounting.employees[2] = "Anna"; 
+
 accounting.describe();
-
-
-const accountingCopy = { describe: accounting.describe, name: "baboon" };
-
-accountingCopy.describe()
+accounting.printEmployeeInformation();
