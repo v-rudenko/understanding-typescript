09cf032233355cc089bb4afbd32f50ba1c6d287f
Viktor <v.rudenko@triacom.ua>
2023-12-15 13:38:33 +0200

    69. Abstract Classes

--- 05-classes-and-interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ 05-classes-and-interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -2,10 +2,15 @@
 
 class Department {
   // name: string;
-  private employees: string[] = [];
+  static fiscalYear = 2020;
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.name = n;
+  }
+
+  static createEmployee(name: string) {
+    return {name: name}
   }
 
   describe(this: Department) {
@@ -26,31 +31,72 @@ class ITDepartment extends Department {
   constructor(id: string, public admins: string[]) {
     super(id, "ACC");
   }
-
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No reports");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      return;
+    }
+    this.addReport(value);
+  }
+
+
   constructor(id: string, private reports: string[]) {
     super(id, "IT");
+    this.lastReport = reports[0];
   }
+
+  addEmployee(name: string): void {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
-    console.log(this.reports)
+    console.log(this.reports);
+  }
+
+  printLastReport() {
+    console.log(this.lastReport);
   }
 }
 
 const it = new ITDepartment("d1", ["Max"]);
 const accounting = new AccountingDepartment("d2", []);
-accounting.addReport("Something went wrong...")
+
+accounting.mostRecentReport = 'Year end report';
+accounting.addReport("Something went wrong...");
+console.log(accounting.mostRecentReport);
+console.log(accounting.printLastReport());
+
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.addEmployee("Baboon");
+
+const employee1 = Department.createEmployee("Max")
+console.log(employee1)
 
 it.addEmployee("Max");
 it.addEmployee("Manu");
 
-console.log(it)
-console.log(accounting)
+console.log(it);
+console.log(accounting);
 
 // accounting.employees[2] = "Anna";
 
