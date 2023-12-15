bf47dd626b375a14ca1d379dc3032e12632e329e
Viktor <v.rudenko@triacom.ua>
2023-12-15 14:35:45 +0200

    70. Singletons & Private Constructors

--- 05-classes-and-interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ 05-classes-and-interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -5,7 +5,7 @@ class Department {
   static fiscalYear = 2020;
   protected employees: string[] = [];
 
-  constructor(private readonly id: string, public name: string) {
+  constructor(protected readonly id: string, public name: string) {
     // this.name = n;
   }
 
@@ -88,6 +88,7 @@ console.log(accounting.printLastReport());
 accounting.addEmployee("Max");
 accounting.addEmployee("Manu");
 accounting.addEmployee("Baboon");
+accounting.describe();
 
 const employee1 = Department.createEmployee("Max")
 console.log(employee1)
