556ae86cbab9d2669289759effee5617dc5bdd06
Viktor <v.rudenko@triacom.ua>
2023-12-27 13:31:22 +0200

    77. Interfaces as Function Types

--- 05-classes-and-interfaces/cl-interfaces-01-starting-setup/src/app.ts
+++ 05-classes-and-interfaces/cl-interfaces-01-starting-setup/src/app.ts
@@ -1,18 +1,37 @@
-interface Person {
-  name: string;
-  age: number;
+interface Named {
+  readonly name: string;
+}
+
+
+interface Greetable extends Named {
+  readonly name: string;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+// type Person = {
+//   name: string;
+//   age: number;
 
-user1 = {
-  name: "Max",
-  age: 30,
-  greet(phrase: string) {
+//   greet(phrase: string): void;
+// }
+
+class Person implements Greetable {
+  name: string;
+  age = 30;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  greet(phrase: string): void {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
 
-user1.greet("Hi there - I am")
+let user1: Greetable;
+
+user1 = new Person("Max");
+console.log(user1)
+
+user1.greet("Hi there - I am");
