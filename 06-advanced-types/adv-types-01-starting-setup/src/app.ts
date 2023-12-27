91a1c3a4c9954d4d1cdc3e169e5e0d53ca3c3cdd
Viktor <v.rudenko@triacom.ua>
2023-12-27 14:54:07 +0200

    86. Type Casting

--- 06-advanced-types/adv-types-01-starting-setup/src/app.ts
+++ 06-advanced-types/adv-types-01-starting-setup/src/app.ts
@@ -15,3 +15,61 @@ const e1: ElevatedEmployee = {
   privileges: ["create-server"],
   startDate: new Date(),
 };
+
+type Combinable = string | number;
+
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  // if ("flyingSpeed" in animal) {
+  //   console.log("Moving with speed: " + animal.flyingSpeed);
+  // }
+  let speed;
+
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+  console.log("Moving with speed: " + speed);
+}
+
+moveAnimal({type: "bird", flyingSpeed: 10})
