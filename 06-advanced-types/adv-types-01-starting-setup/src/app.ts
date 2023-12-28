75b799b61c3b6e8ba35b6e2ebb2f0d1d8a1fa4c0
Viktor <v.rudenko@triacom.ua>
2023-12-28 13:20:52 +0200

    pause

--- 06-advanced-types/adv-types-01-starting-setup/src/app.ts
+++ 06-advanced-types/adv-types-01-starting-setup/src/app.ts
@@ -73,3 +73,10 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({type: "bird", flyingSpeed: 10})
+
+// const userInputElement = <HTMLInputElement>document.getElementById("user-input")!;
+
+const userInputElement = document.getElementById("user-input")! as HTMLInputElement;
+
+userInputElement.value = "hi there!";
+
