f1fd62bec0a8f84c7d4058e5b202748e05d0aa3b
Viktor <v.rudenko@triacom.ua>
2024-01-17 16:31:00 +0200

    99. Generic Classes

--- 07-generics/generics-01-starting-setup/src/app.ts
+++ 07-generics/generics-01-starting-setup/src/app.ts
@@ -36,3 +36,14 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 }
 
 console.log(countAndDescribe([]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+const result = extractAndConvert({ name: "max" }, "name");
+
+console.log(result);
