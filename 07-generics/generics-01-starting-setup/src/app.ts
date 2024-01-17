c1faca681f947f9c7999c360326b1fff0f01c1a3
Viktor <v.rudenko@triacom.ua>
2024-01-17 15:31:42 +0200

    95. Creating a Generic Function

---
+++ 07-generics/generics-01-starting-setup/src/app.ts
@@ -0,0 +1,13 @@
+// const names: Array<string> = ["Max", "Manuel"]
+
+// const promise: Promise<string> = new Promise((resolve) => {
+//   setTimeout(() => {
+//     console.log("bobbi")
+//     resolve("This is done!");
+//     console.log("bobun");
+//   }, 2000)
+// });
+
+// promise.then(data => {
+//   data.split(" ");
+// })
