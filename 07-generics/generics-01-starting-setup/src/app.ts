bc4f58787a0a0ef13ae9905bbc8387a4084d6256
Viktor <v.rudenko@triacom.ua>
2024-01-17 16:01:04 +0200

    98. The "keyof" Constraint

--- 07-generics/generics-01-starting-setup/src/app.ts
+++ 07-generics/generics-01-starting-setup/src/app.ts
@@ -12,10 +12,27 @@
 //   data.split(" ");
 // })
 
-function merge<T, U>(objA: T, objB: U) {
-  return {...objA, ...objB};
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return { ...objA, ...objB };
 }
 
-const mergedObj = merge({name: "Max"}, {age: 30});
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 
-mergedObj.age;
+console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element";
+  }
+  if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe([]));
